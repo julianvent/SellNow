a77bab9f55cdffc4e90756b3278535364bc1dfb5
Julian <zS22017046@estudiantes.uv.mx>
2024-10-05 00:50:44 -0600

    Login setup in ViewFactory. Customs fonts added to UI

--- src/main/java/jjvu/projects/sellnow/App.java
+++ src/main/java/jjvu/projects/sellnow/App.java
@@ -1,4 +1,18 @@
 package jjvu.projects.sellnow;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import jjvu.projects.sellnow.views.ViewFactory;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory();
+        viewFactory.showLoginWindow();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/jjvu/projects/sellnow/views/ViewFactory.java
+++ src/main/java/jjvu/projects/sellnow/views/ViewFactory.java
@@ -1,4 +1,38 @@
 package jjvu.projects.sellnow.views;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ViewFactory {
+    public ViewFactory() {}
+
+    /*
+    Login Window
+     */
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        createStage(loader);
+    }
+
+
+    /*
+    Utility
+     */
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("SellNow");
+        stage.show();
+    }
 }
