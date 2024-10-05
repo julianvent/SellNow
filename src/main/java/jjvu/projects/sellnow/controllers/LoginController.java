ee499feac8b1cbc11d36ec7e117460f65de6316c
Julian <zS22017046@estudiantes.uv.mx>
2024-10-05 16:25:21 -0600

    SideBar controller updated. Also initializable interface implemented in controller

--- src/main/java/jjvu/projects/sellnow/controllers/LoginController.java
+++ src/main/java/jjvu/projects/sellnow/controllers/LoginController.java
@@ -1,12 +1,21 @@
 package jjvu.projects.sellnow.controllers;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public TextField usernameField;
     public TextField passwordField;
     public Button loginButton;
     public Label errorLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/jjvu/projects/sellnow/controllers/SiderBarController.java
+++ src/main/java/jjvu/projects/sellnow/controllers/SiderBarController.java
@@ -1,4 +1,22 @@
 package jjvu.projects.sellnow.controllers;
 
-public class SiderBarController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SiderBarController implements Initializable {
+    public Button mainMenuButton;
+    public Button userButton;
+    public Button logoutButton;
+    public Button createSaleButton;
+    public Button listSalesButton;
+    public Button createProductButton;
+    public Button listProductsButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
