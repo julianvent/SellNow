2ff4fa0637d356ad4f498b72a8c2dbf7d9fe7f2f
Julian <zS22017046@estudiantes.uv.mx>
2024-10-05 17:18:59 -0600

    Dashboard controller updated

--- src/main/java/jjvu/projects/sellnow/controllers/DashboardController.java
+++ src/main/java/jjvu/projects/sellnow/controllers/DashboardController.java
@@ -1,4 +1,22 @@
 package jjvu.projects.sellnow.controllers;
 
-public class DashboardController {
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    public Label shopNameLabel;
+    public Label currentDateLabel;
+    public FontAwesomeIconView fastSaleCreateButton;
+    public Label totalImportLabel;
+    public ListView lastSalesListView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
