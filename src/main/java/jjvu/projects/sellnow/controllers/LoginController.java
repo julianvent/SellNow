c99a3116e8baee285d0a5de9b2040b5a25c3bb5a
Julian <zS22017046@estudiantes.uv.mx>
2024-10-04 22:38:31 -0600

    Login fxml and controller updated

--- src/main/java/jjvu/projects/sellnow/controllers/LoginController.java
+++ src/main/java/jjvu/projects/sellnow/controllers/LoginController.java
@@ -1,4 +1,12 @@
 package jjvu.projects.sellnow.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField usernameField;
+    public TextField passwordField;
+    public Button loginButton;
+    public Label errorLabel;
 }
