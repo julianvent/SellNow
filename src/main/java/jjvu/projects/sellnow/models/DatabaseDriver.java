341b91568086d7fe1128f7a35681ff3a204e3fb5
Julian <zS22017046@estudiantes.uv.mx>
2024-10-06 22:44:40 -0600

    DatabaseDriver added with getUserData method

---
+++ src/main/java/jjvu/projects/sellnow/models/DatabaseDriver.java
@@ -0,0 +1,35 @@
+package jjvu.projects.sellnow.models;
+
+import java.sql.*;
+
+public class DatabaseDriver {
+    private final Connection connection;
+
+    public DatabaseDriver() {
+        try {
+            connection = DriverManager.getConnection("jbbc:sqlite:sellnow.db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+    User
+     */
+    public ResultSet getUserData(String username, String password) {
+        Statement statement;
+        ResultSet resultSet;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(String.format(
+                    "SELECT * FROM %s WHERE username=%s AND password=%s"
+                    , "user", username, password)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return resultSet;
+    }
+}
